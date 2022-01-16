37714bdefd75b0938ba2852f21fb66f4f0646a7e
Thiago Waldrich <thiagowlf7@gmail.com>
2022-01-16 17:10:18 -0300

    Alteracao final

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -1,12 +1,12 @@
 import MovieScore from "components/MovieScore";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import { Movie } from "types/movie";
 
 type Props = {
     movie: Movie;
 }
 
-function MovieCard({movie} : Props) {
+function MovieCard({ movie }: Props) {
 
     return (
         <div>
@@ -14,11 +14,11 @@ function MovieCard({movie} : Props) {
             <div className="dsmovie-card-bottom-container">
                 <h3>{movie.title}</h3>
                 <MovieScore count={movie.count} score={movie.score} /> {/* ctrl + espaço */}
-                <Link to="/form/1">
-                    <div className="btn btn-primary dsmovie-btn">Avaliar</div>
-                </Link>
-            </div>
+                <Link to={`/form/${movie.id}`}>
+                <div className="btn btn-primary dsmovie-btn">Avaliar</div>
+            </Link>
         </div>
+        </div >
     );
 }
 
