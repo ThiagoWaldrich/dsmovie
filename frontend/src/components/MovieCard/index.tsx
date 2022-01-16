496ae1c202c3d1519a5c7f33989c088107bb5be1
Thiago Waldrich <thiagowlf7@gmail.com>
2022-01-16 16:05:30 -0300

    Show score

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -13,7 +13,7 @@ function MovieCard({movie} : Props) {
             <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />
             <div className="dsmovie-card-bottom-container">
                 <h3>{movie.title}</h3>
-                <MovieScore /> {/* ctrl + espaço */}
+                <MovieScore count={movie.count} score={movie.score} /> {/* ctrl + espaço */}
                 <Link to="/form/1">
                     <div className="btn btn-primary dsmovie-btn">Avaliar</div>
                 </Link>
